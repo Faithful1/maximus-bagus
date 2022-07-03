e5751442a1f5c79bc3c70d3baffec0fa47df8a63
faithfulanere1 <faithful.anere@strongarmtech.com>
2022-07-03 17:12:57 +0200

    Create  Genesis ECR repository

--- Genesis/Infrastructure/resources/compute/aws-ecr/output.tf
+++ Genesis/Infrastructure/resources/compute/aws-ecr/output.tf
@@ -1,37 +1,3 @@
-# USE THIS FOR STAGING
-
-# output "repository_url" {
-#     value = aws_ecr_repository.this[0].repository_url
-# }
-
-# //change this to * when on staging while choose [0] when deploying to prod
-# output "prod_repository_url" {
-#     value = aws_ecr_repository.this_task_prod[*].repository_url
-# }
-
-
-
-
-// USE THIS FOR PROD
-
-# output "repository_url" {
-#     value = aws_ecr_repository.this[*].repository_url
-# }
-
-# output "prod_repository_url" {
-#     value = aws_ecr_repository.this_task_prod[0].repository_url
-# }
-
-
-
-
-# UNDO FOR CMS DEPLOY
-
 output "repository_url" {
-  value = aws_ecr_repository.this[*].repository_url
-}
-
-//change this to * when on staging while choose [0] when deploying to prod
-output "prod_repository_url" {
-  value = aws_ecr_repository.this_task_prod[*].repository_url
+  value = aws_ecr_repository.this.repository_url
 }
